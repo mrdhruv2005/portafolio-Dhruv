0f643dc23feebf96c5f6b177027ee74730d17d02
Your Name <dhruvgarg4848@gmail.com>
2026-06-02 11:45:05 +0530

    Configure Vercel deployment

--- portafolio-Dhruv/src/components/Footer/Footer.jsx
+++ portafolio-Dhruv/src/components/Footer/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () =>{
                     <p>
                         <FormattedMessage
                             id='footer-info'
-                            defaultMessage='Page created by Nahuel61920'
+                            defaultMessage='Page created by Dhruv Garg'
                         />
                     </p>
                     <p>&copy; {fecha}. All Rights Reserved.</p>
